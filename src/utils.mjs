9396a46199f79520ee85bf9108687c85da86d554
Conor Walton <waltoncon@users.noreply.github.com>
2022-05-23 07:31:37 +0100

    feat: add compare method util for array sort

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,3 +1,5 @@
 export const wrap = (s, w) => s.replace(
     new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n',
 );
+
+export const compare = key => (a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0);
